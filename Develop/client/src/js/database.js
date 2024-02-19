f28730a07011f92c87b2b13b75d27ae7a5119ef5
Kalleepar <kalleepar@gmail.com>
2024-02-19 12:57:15 -0500

    added function to install and database js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    // Database/Version
+    const jateDB = await openDB("jate", 1);
+    const tx = jateDB.transaction("jate", "readwrite");
+    // Open specific object store
+    const store = tx.objectStore("jate");
+    // Put content in store
+    const request = store.put({ id: 1, value: content });
+    // Confirm test
+    const result = await request;
+    console.log("data saved to the database", result);
+  };
+  
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    // Database/Version
+    const jateDB = await openDB("jate", 1);
+    // New transaction
+    const tx = jateDB.transaction("jate", "readonly");
+   // Open specific object store
+    const store = tx.objectStore("jate");
+    // Get all requests
+    const request = store.getAll();
+    // Confirm test
+    const result = await request;
+    console.log("🚀 - data read from database", result);
+    return result.value;
+  };
 
 initdb();
